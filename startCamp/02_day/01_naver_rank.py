cd9acecfb87b8bd72cb633093c248618e36d0b6d
SINHOLEE <dltlsgh5@naver.com>
2019-07-09 13:02:44 +0900

    Start camp day 02

---
+++ startCamp/02_day/01_naver_rank.py
@@ -0,0 +1,25 @@
+import requests
+import select
+import bs4
+
+
+url = "https://www.naver.com/"
+selector = '.ah_k'
+response = requests.get(url)
+html = response.text
+
+soup = bs4.BeautifulSoup(html, 'html.parser')
+
+top_10 = soup.select(selector)
+
+
+
+#우리가 원하는 정보는 급상승 인기검색어 -> 우클릭 -> 소스보기
+
+#'.ah_l .ah_item .ah_a .ah_k # 셀렉터 입니다?'
+
+
+# for item in top_10 : 
+#     print(item.text)
+
+print(html)
