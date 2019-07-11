d9749836df37aac2b8668272dcef5a556dd35843
SINHOLEE <dltlsgh5@naver.com>
2019-07-11 18:55:02 +0900

    show me the result of lotto when i choose a round

--- startCamp/04_day/FLask/app.py
+++ startCamp/04_day/FLask/app.py
@@ -62,23 +62,61 @@ def lotto_result():
     lotto_round = request.args.get('lotto_round')
     lotto_nums = request.args.get('nums').split(" ")
     for i in range(len(lotto_nums)) :
-        lotto_nums[i] = int(i)
+        lotto_nums[i] = int(lotto_nums[i])
     print(lotto_nums)
     url = f'https://dhlottery.co.kr/common.do?method=getLottoNumber&drwNo={lotto_round}'
 
     response = requests.get(url)
     #json형식의 자료를 가져올때
     lotto_infos = response.json() # json 타입의 파일을 python dictionary 로 parsing해줘
+    
+
+#밑으로 내가했당 기분좋아
+    
     for key, value in lotto_infos.items() :
-        print(key, type(value))
+        print(key, value)
     for num in lotto_nums :
         print(type(num))
+    bnus_No = lotto_infos['bnusNo']
+    print(bnus_No)
     
 
+    lotto_infos_list = []
+    for i in range(1,7) :
+        lotto_infos_list.append(lotto_infos[f'drwtNo{i}'])
+    print("몇 회차 당첨번호", lotto_infos_list)
+    print('내가 선택한 번호', lotto_nums)
+    print('보너스 번호', bnus_No)
+    got_set = set(lotto_infos_list)
+    got_set.add(bnus_No) # got_set = 당첨이 끝난 7개 번호
+    num_set = set(lotto_nums) # num_set = 내가 선택한 6개 번호
+    print(got_set) 
+    print(num_set)    
+    count = 0
+    for i in got_set :
+        if i in num_set :
+            count += 1
+        else:
+            pass
+
+    
+    if count == 6 and bnus_No not in got_set :
+        final = "축하합니다 1등입니다."
+    elif count == 6 and bnus_No in got_set :
+        final = '축하합니다 2등입니다.'
+    elif count == 5 :
+        final = '축하합니다 3등입니다.'
+    elif count == 4 :
+        final = '축하합니다 4등입니다.'
+    else :
+        final = '아쉽지만 다음 기회에'
+    
+   
 
     
     
-    return f'{lotto_round}, {lotto_nums}'
+    
+    return f'{lotto_round}회차, 당첨번호는{lotto_nums} 입니다. {final}'
 
 
 if __name__ =='__main__':   # 임포트해서 실행하면 네임은 이름이 되고, python으로 실행하면 name이 main이 된다.
